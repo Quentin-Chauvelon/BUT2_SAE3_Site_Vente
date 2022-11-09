d1bf59b28e3e07a62682e41f890a7e7e4c141eb1
E219111P <louis.bruneteau@univ-nantes.fr>
2022-11-09 09:03:54 +0100

    CREATE or replace

--- app/Database/Seeds/create_tables.sql
+++ app/Database/Seeds/create_tables.sql
@@ -1,4 +1,4 @@
-CREATE TABLE Client(
+CREATE OR REPLACE TABLE Client(
     id_client INT PRIMARY KEY,
     adresse_email VARCHAR(255) UNIQUE NOT NULL,
     nom VARCHAR(255) NOT NULL,
@@ -8,10 +8,10 @@ CREATE TABLE Client(
     est_fidele BOOLEAN NOT NULL DEFAULT false -- TODO ajouter avec un trigger
 );
 
-CREATE VIEW Admin AS SELECT * FROM Client WHERE est_admin = true;
-CREATE VIEW Fidele AS SELECT * FROM Client WHERE est_fidele = true;
+CREATE OR REPLACE VIEW Admin AS SELECT * FROM Client WHERE est_admin = true;
+CREATE OR REPLACE VIEW Fidele AS SELECT * FROM Client WHERE est_fidele = true;
 
-CREATE TABLE Coupon(
+CREATE OR REPLACE TABLE Coupon(
     code VARCHAR(20) PRIMARY KEY,
     nom VARCHAR(50) NOT NULL,
     montant INT NOT NULL CHECK(montant > 0),
@@ -20,7 +20,7 @@ CREATE TABLE Coupon(
     CONSTRAINT pourcentage_valable CHECK(NOT est_pourcentage OR montant <= 100)
 );
 
-CREATE TABLE Commande(
+CREATE OR REPLACE TABLE Commande(
     id_commande INT PRIMARY KEY,
     id_client INT NOT NULL,
     date_commande DATE NOT NULL,
@@ -33,14 +33,14 @@ CREATE TABLE Commande(
     CONSTRAINT livraison_apres_commande CHECK(date_livraison IS NULL OR date_livraison > date_commande)
 );
 
-CREATE TABLE Collection (
+CREATE OR REPLACE TABLE Collection (
     id_collection INT PRIMARY KEY,
     parution DATE NOT NULL,
     date_limite DATE,
     CONSTRAINT fin_apres_parution CHECK(date_limite IS NULL OR date_limite > parution)
 );
 
-CREATE TABLE Produit(
+CREATE OR REPLACE TABLE Produit(
     id_produit INT PRIMARY KEY,
     id_collection INT,
     nom VARCHAR(100) UNIQUE NOT NULL,
@@ -54,16 +54,16 @@ CREATE TABLE Produit(
     CONSTRAINT check_prix_superieur_reduction CHECK(reduction <= prix),
 );
 
-CREATE VIEW ProduitReduction AS SELECT * FROM Produit WHERE reduction > 0;
+CREATE OR REPLACE VIEW ProduitReduction AS SELECT * FROM Produit WHERE reduction > 0;
 
-CREATE VIEW Poster AS SELECT * FROM Produit WHERE categorie = "poster";
-CREATE VIEW Accessoire AS SELECT * FROM Produit WHERE categorie = "accessoire";
-CREATE VIEW Vetement AS SELECT * FROM Produit WHERE categorie in ("pantalon", "sweat", "tshirt");
-CREATE VIEW Pantalon AS SELECT * FROM Produit WHERE categorie = "pantalon";
-CREATE VIEW Sweat AS SELECT * FROM Produit WHERE categorie = "sweat";
-CREATE VIEW Tshirt AS SELECT * FROM Produit WHERE categorie = "tshirt";
+CREATE OR REPLACE VIEW Poster AS SELECT * FROM Produit WHERE categorie = "poster";
+CREATE OR REPLACE VIEW Accessoire AS SELECT * FROM Produit WHERE categorie = "accessoire";
+CREATE OR REPLACE VIEW Vetement AS SELECT * FROM Produit WHERE categorie in ("pantalon", "sweat", "tshirt");
+CREATE OR REPLACE VIEW Pantalon AS SELECT * FROM Produit WHERE categorie = "pantalon";
+CREATE OR REPLACE VIEW Sweat AS SELECT * FROM Produit WHERE categorie = "sweat";
+CREATE OR REPLACE VIEW Tshirt AS SELECT * FROM Produit WHERE categorie = "tshirt";
 
-CREATE TABLE Exemplaire(
+CREATE OR REPLACE TABLE Exemplaire(
     id_exemplaire INT PRIMARY KEY,
     id_produit INT NOT NULL,
     id_commande INT, -- TODO trigger pour vérifier si la date d'obtention correspond à la date de commande
@@ -75,9 +75,9 @@ CREATE TABLE Exemplaire(
     CONSTRAINT dispo_pas_commande CHECK(id_commande IS NULL OR est_disponible = false) -- empêche l'article d'être dispo alors qu'il est commandé
 );
 
-CREATE VIEW ExemplaireDispo AS SELECT * FROM Exemplaire WHERE est_disponible = true;
+CREATE OR REPLACE VIEW ExemplaireDispo AS SELECT * FROM Exemplaire WHERE est_disponible = true;
 
-CREATE TABLE Favori(
+CREATE OR REPLACE TABLE Favori(
     id_client INT NOT NULL,
     id_produit INT NOT NULL ,
     CONSTRAINT fk_favori_client FOREIGN KEY(id_client) REFERENCES Client(id_client),

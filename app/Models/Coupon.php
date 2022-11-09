333e98bf3d0364a13896967c6688673b14b907c9
E205240M <arthur.osselin@etu.univ-nantes.fr>
2022-11-09 11:34:16 +0100

    qzd

---
+++ app/Models/Coupon.php
@@ -0,0 +1,40 @@
+<?php
+class Coupon
+{
+    /** @var string L'identifiant unique du coupon
+     * 
+     * 
+     */
+    private $code;
+
+    /** @var string le nom du coupon
+     * 
+     * 
+     */
+    private $nom;
+
+
+    /** @var int le montant du coupon
+     * 
+     * 
+     */
+    private $montant;
+
+    /** @var bool indique si le coupon est en pourcentage
+     * 
+     * 
+     */
+    private $est_pourcentage;
+
+    /** @var bool  indique si le coupon est valable
+     * 
+     * 
+     */
+    private $est_valable;
+
+
+
+
+}
+
+?>

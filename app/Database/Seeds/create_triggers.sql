656a10893095d9fde9cf7c1402adf38a51de1c65
LBruneteau <bruneteau.louis@gmail.com>
2022-11-16 14:43:07 +0100

    Je suis absolument désespéré que la mort m'emporte

--- app/Database/Seeds/create_triggers.sql
+++ app/Database/Seeds/create_triggers.sql
@@ -1,10 +1,17 @@
 -- COUPON TROP UTILISÉ
 
 -- COUPON EXPIRÉ
-
-CREATE OR REPLACE TRIGGER coupon_trop_utilise_insert
+CREATE TRIGGER coupon_trop_utilise_insert
 BEFORE INSERT ON Commande FOR EACH ROW BEGIN
-	IF NEW.id_coupon IS NOT NULL AND (SELECT MAX(c.utilisations_max) FROM Coupon c WHERE c.id_coupon = NEW.id_coupon) <= (SELECT COUNT(*) FROM Commande c WHERE c.id_coupon = NEW.id_coupon)
+IF new.id_coupon IS NOT NULL AND
+(SELECT MAX(utilisations_max) FROM Coupon WHERE code = NEW.id_coupon) <=
+(SELECT COUNT(*) FROM Commande WHERE id_coupon = NEW.id_coupon) THEN
+	SIGNAL SQLSTATE '45000' SET message_text = 'Ce coupon de réduction a été trop souvent utilisé.';
+END IF;
+END;
+/* CREATE TRIGGER coupon_trop_utilise_insert
+BEFORE INSERT ON Commande FOR EACH ROW BEGIN
+	IF NEW.id_coupon IS NOT NULL AND (SELECT MAX(c.utilisations_max) FROM Coupon c WHERE c.code = NEW.id_coupon) <= (SELECT COUNT(*) FROM Commande c WHERE c.id_coupon = NEW.id_coupon)
 	THEN
 		SIGNAL SQLSTATE '45000' SET message_text = 'Ce coupon de réduction a été trop souvent utilisé.';
 	END IF;
@@ -18,7 +25,7 @@ BEFORE UPDATE ON Commande FOR EACH ROW BEGIN
 		THEN
 			SIGNAL SQLSTATE '45000' SET message_text = 'Ce coupon de réduction a été trop souvent utilisé.';
 	END IF;
-END;
+END; */
 
 /* CREATE OR REPLACE TRIGGER coupon_expire_insert
 BEFORE INSERT ON Commande FOR EACH ROW

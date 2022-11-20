057942d6afd01b5d72c7d16cf0b8f2a83482dc0d
Louis Bruneteau <bruneteau.louis@gmail.com>
2022-11-20 13:09:53 +0100

    procédures sur client collection et coupon

--- app/Database/Seeds/create_procedures.sql
+++ app/Database/Seeds/create_procedures.sql
@@ -1,4 +1,5 @@
 /*
+### Table Client
 - GetAllClients()
 - GetClientParID(ID)
 - GetClientParEmail(email)
@@ -6,61 +7,90 @@
 - GetAllFideles()
 - CreerClient(mail, nom, prenom, password) Pas admin par défaut
 - ModifierClient(id, mail, nom, prenom, password, admin)
-- CreerCollection(nom)
+- SupprimerClient(id)
+
+### Table Collection
+- CreerCollection(nom) Parution aujourd'hui et pas de date limite par défaut
 - ModifierCollection(id, nom, parution, date_limite)
 - GetAllCollections()
 - GetCollectionParId(id)
 - GetCollectionParNom(nom)
 - GetCollectionsActuelles()
 - GetCollectionsEphemeresActuelles()
+- SupprimerCollection(id)
+
+### Table Coupon
+- CreerCoupon(code, nom, montant, est_pourcentage, est_valable) Pas de date limite ni de nombre d'utilisations max par défaut.
+- ModifierCoupon(code, nom, montant, est_pourcentage, est_valable, date_limite, utilisations_max)
+- GetAllCoupons()
+- GetAllCouponsValables()
+- GetCouponParCode(code)
+- NombreUtilisationsCoupon(code)
+- GetAllCouponsNonExpires()
+- GetAllCouponsUtilisables()
+- SupprimerCoupon(code)
 */
+
 
 CREATE OR REPLACE PROCEDURE GetAllClients()
 BEGIN
-   SELECT * FROM Client;
+    SELECT * FROM Client;
 END;
 
-CREATE OR REPLACE PROCEDURE GetClientParID(IN ID INT)
+CREATE OR REPLACE PROCEDURE GetClientParID(IN _id_client INT)
 BEGIN
-    SELECT * FROM Client WHERE id_client = ID;
+    SELECT * FROM Client WHERE id_client = _id_client;
 END;
 
-CREATE OR REPLACE PROCEDURE GetClientParEmail(IN mail VARCHAR(255))
+CREATE OR REPLACE PROCEDURE GetClientParEmail(IN _adresse_email VARCHAR(255))
 BEGIN
-    SELECT * FROM Client WHERE adresse_email=mail;
+    SELECT * FROM Client WHERE adresse_email = _adresse_email;
 END;
 
 CREATE OR REPLACE PROCEDURE GetAllAdmins()
 BEGIN
-   SELECT * FROM Admin;
+    SELECT * FROM Admin;
 END;
 
 CREATE OR REPLACE PROCEDURE GetAllFideles()
 BEGIN
-  SELECT * FROM Fidele;
+    SELECT * FROM Fidele;
 END;
 
 CREATE OR REPLACE PROCEDURE CreerClient(
-IN new_mail VARCHAR(255), IN new_nom VARCHAR(255), IN new_prenom VARCHAR(64), IN new_password VARCHAR(64))
+    IN _adresse_email VARCHAR(255), IN _nom VARCHAR(255), IN _prenom VARCHAR(64), IN _password VARCHAR(64))
 BEGIN
-   INSERT INTO Client(adresse_email, nom, prenom, password, est_admin) VALUES (new_mail, new_nom, new_prenom, new_password, false);
+    INSERT INTO Client(adresse_email, nom, prenom, password, est_admin)
+    VALUES (_adresse_email, _nom, _prenom, _password, false);
 END;
 
 CREATE OR REPLACE PROCEDURE ModifierClient(
-IN ID INT, IN new_mail VARCHAR(255), IN new_nom VARCHAR(255), IN new_prenom VARCHAR(64), IN new_password VARCHAR(64), IN new_admin BOOLEAN)
+    IN _id_client INT, IN _adresse_email VARCHAR(255), IN _nom VARCHAR(255), IN _prenom VARCHAR(64),
+    IN _password VARCHAR(64), IN _est_admin BOOLEAN)
 BEGIN
-    UPDATE Client SET adresse_email=new_mail, nom=new_nom, prenom=new_prenom, password=new_password, est_admin=new_admin WHERE id_client=ID;
+    UPDATE Client
+    SET adresse_email=_adresse_email,
+        nom=_nom,
+        prenom=_prenom,
+        password=_password,
+        est_admin=_est_admin
+    WHERE id_client = _id_client;
 END;
 
-
-CREATE OR REPLACE PROCEDURE CreerCollection(IN nom_coll VARCHAR(50))
+CREATE OR REPLACE PROCEDURE SupprimerClient(IN _id_client INT)
 BEGIN
-    INSERT INTO Collection(nom, parution, date_limite) VALUES (nom_coll, CURDATE(), NULL);
+   DELETE FROM Client WHERE id_client=_id_client;
 END;
 
-CREATE OR REPLACE PROCEDURE ModifierCollection(IN ID INT, nom_coll VARCHAR(50), debut DATE, date_fin DATE)
+CREATE OR REPLACE PROCEDURE CreerCollection(IN _nom VARCHAR(50))
 BEGIN
-    UPDATE Collection SET nom=nom_coll, date_limite=date_fin, parution=debut WHERE id_collection=ID;
+    INSERT INTO Collection(nom, parution, date_limite) VALUES (_nom, CURDATE(), NULL);
+END;
+
+CREATE OR REPLACE PROCEDURE ModifierCollection(IN _id_collection INT, _nom VARCHAR(50), _parution DATE,
+                                               _date_limite DATE)
+BEGIN
+    UPDATE Collection SET nom=_nom, date_limite=_date_limite, parution=_parution WHERE id_collection = _id_collection;
 END;
 
 CREATE OR REPLACE PROCEDURE GetAllCollections()
@@ -68,15 +98,14 @@ BEGIN
     SELECT * FROM Collection;
 END;
 
-
-CREATE OR REPLACE PROCEDURE GetCollectionParId(IN id INT)
+CREATE OR REPLACE PROCEDURE GetCollectionParId(IN _id_collection INT)
 BEGIN
-    SELECT * FROM Collection WHERE id_collection=id;
+    SELECT * FROM Collection WHERE id_collection = _id_collection;
 END;
 
-CREATE OR REPLACE PROCEDURE GetCollectionParNom(IN nom_coll VARCHAR(50))
+CREATE OR REPLACE PROCEDURE GetCollectionParNom(IN _nom VARCHAR(50))
 BEGIN
-    SELECT * FROM Collection WHERE nom=nom_coll;
+    SELECT * FROM Collection WHERE nom = _nom;
 END;
 
 CREATE OR REPLACE PROCEDURE GetCollectionsActuelles()
@@ -86,5 +115,70 @@ END;
 
 CREATE OR REPLACE PROCEDURE GetCollectionsEphemeresActuelles()
 BEGIN
-   SELECT * FROM Collection WHERE date_limite IS NOT NULL AND date_limite >= CURDATE();
+    SELECT * FROM Collection WHERE date_limite IS NOT NULL AND date_limite >= CURDATE();
+END;
+
+CREATE OR REPLACE PROCEDURE SupprimerCollection(IN _id_collection INT)
+BEGIN
+    DELETE FROM Collection WHERE id_collection=_id_collection;
+END;
+
+CREATE OR REPLACE PROCEDURE CreerCoupon(
+    IN _code VARCHAR(20),
+    IN _nom VARCHAR(50),
+    IN _montant INT,
+    IN _est_pourcentage BOOLEAN,
+    IN _est_valable BOOLEAN)
+BEGIN
+    INSERT INTO Coupon(code, nom, montant, est_pourcentage, est_valable, date_limite, utilisations_max) VALUES
+    (_code, _nom, _montant, _est_pourcentage, _est_valable, NULL, NULL);
+END;
+
+CREATE OR REPLACE PROCEDURE ModifierCoupon(
+    IN _code VARCHAR(20),
+    IN _nom VARCHAR(50),
+    IN _montant INT,
+    IN _est_pourcentage BOOLEAN,
+    IN _est_valable BOOLEAN,
+    IN _date_limite DATE,
+    IN _utilisations_max INT)
+BEGIN
+   UPDATE Coupon SET nom=_nom, montant=_montant, est_pourcentage=_est_pourcentage,
+    est_valable=_est_valable, date_limite=_date_limite, utilisations_max=_utilisations_max WHERE code=_code;
+END;
+
+CREATE OR REPLACE PROCEDURE GetAllCoupons()
+BEGIN
+   SELECT * FROM Coupon;
+END;
+
+CREATE OR REPLACE PROCEDURE GetAllCouponsValables()
+BEGIN
+   SELECT * FROM CouponValable;
+END;
+
+CREATE OR REPLACE PROCEDURE GetCouponParCode(IN _code VARCHAR(20))
+BEGIN
+    SELECT * FROM Coupon WHERE code=_code;
+END;
+
+CREATE OR REPLACE PROCEDURE NombreUtilisationsCoupon(IN _code VARCHAR(20))
+BEGIN
+    SELECT COUNT(*) AS nombre_utilisations FROM Commande where id_coupon=_code;
+END;
+
+CREATE OR REPLACE PROCEDURE GetAllCouponsNonExpires()
+BEGIN
+   SELECT * FROM Coupon WHERE date_limite IS NULL OR date_limite >= CURDATE();
+END;
+
+CREATE OR REPLACE PROCEDURE GetAllCouponsUtilisables()
+BEGIN
+    SELECT * FROM Coupon WHERE est_valable AND (date_limite IS NULL OR date_limite >= CURDATE())
+    AND (utilisations_max IS NULL OR utilisations_max > (SELECT COUNT(*) FROM Commande where id_coupon=code));
+END;
+
+CREATE OR REPLACE PROCEDURE SupprimerCoupon(IN _code VARCHAR(20))
+BEGIN
+   DELETE FROM Coupon WHERE code=_code;
 END;

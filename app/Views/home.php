f83db9df6cd8b3d24fd73aedeae6cdd60b3f7caa
Louis Bruneteau <bruneteau.louis@gmail.com>
2022-11-24 09:12:00 +0100

    marche par satan

--- app/Views/home.php
+++ app/Views/home.php
@@ -5,7 +5,7 @@
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <script src="home.js"></script>
-  <link rel="stylesheet" href="../../../assets/css/home.css">
+  <link rel="stylesheet" href="<?= base_url('assets/css/home.css') ?>">
   <title>Hot genre</title>
 </head>
 

700a41ba0c675ee5f21e03b7cc35db346f451792
E205240M <arthur.osselin@etu.univ-nantes.fr>
2022-11-24 08:53:19 +0100

    home controller

--- app/Controllers/Home.php
+++ app/Controllers/Home.php
@@ -9,11 +9,19 @@ class Home extends BaseController
 	// 	$this->load->helper('url');
 	// }
 
+<<<<<<< HEAD
     public function index()
     {/*
         $this->load->model('ProductModel');
+=======
+    private ProductModel $ProductModel;
+
+    public function index(): string
+    {
+/*        $this->ProductModel = model('ProductModel');
+>>>>>>> 122553f2beee608e938dcb7980bfe1777d64face
         $products = $this->ProductModel->findAll();
-		$data = array("prods"=>$products);
+		$data = array("prods"=>$products);*/
 
 		// Les 3 plus populaires
 		// Les images de la dernière collection

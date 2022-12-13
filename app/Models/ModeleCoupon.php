656c5476a95898ba48f78aa08830792c82d388c3
Louis Bruneteau <bruneteau.louis@gmail.com>
2022-12-13 14:24:02 +0100

    coupon

--- app/Models/ModeleCoupon.php
+++ app/Models/ModeleCoupon.php
@@ -2,41 +2,28 @@
 
 namespace App\Models;
 
+use App\Entities\Coupon;
 use CodeIgniter\Model;
 
 class ModeleCoupon extends Model
 {
-    protected $DBGroup          = 'default';
-    protected $table            = 'modelecoupons';
-    protected $primaryKey       = 'id';
-    protected $useAutoIncrement = true;
-    protected $insertID         = 0;
-    protected $returnType       = 'array';
-    protected $useSoftDeletes   = false;
-    protected $protectFields    = true;
-    protected $allowedFields    = [];
+    private static ModeleCoupon $instance;
 
-    // Dates
-    protected $useTimestamps = false;
-    protected $dateFormat    = 'datetime';
-    protected $createdField  = 'created_at';
-    protected $updatedField  = 'updated_at';
-    protected $deletedField  = 'deleted_at';
+    protected $table            = 'Coupon';
+    protected $primaryKey       = 'id_coupon';
+    protected $returnType       = Coupon::class;
+    protected $allowedFields    = ['id_coupon', 'nom', 'montant', 'est_pourcentage', 'est_valable', 'date_limite', 'utilisations_max'];
 
-    // Validation
-    protected $validationRules      = [];
-    protected $validationMessages   = [];
-    protected $skipValidation       = false;
-    protected $cleanValidationRules = true;
+    private function __construct()
+    {
+        parent::__construct();
+    }
 
-    // Callbacks
-    protected $allowCallbacks = true;
-    protected $beforeInsert   = [];
-    protected $afterInsert    = [];
-    protected $beforeUpdate   = [];
-    protected $afterUpdate    = [];
-    protected $beforeFind     = [];
-    protected $afterFind      = [];
-    protected $beforeDelete   = [];
-    protected $afterDelete    = [];
+    public static function getInstance(): ModeleCoupon
+    {
+        if (!isset(self::$instance)) {
+            self::$instance = new ModeleCoupon();
+        }
+        return self::$instance;
+    }
 }
